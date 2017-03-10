bed2935061b6cd62d4d4718161ad1ad6be043d27
nsrathor13 <nrathor@oswego.edu>
2017-03-10 15:47:36 -0500

    my first commit

--- main.java
+++ main.java
@@ -1,48 +1,7 @@
-File: hw2.clj
-;Authors: Dor Rondel and Nirender
+public class main{
+	
+	public static void main(String[] args) {
+	System.out.println("Hello Nirender's World");
 
-(ns hw2)
-
-;Redefined orexp and andexp to take variable # of parameters
-(defn orexp [& args]
-  (reduce
-    (fn [e1 e2]
-      (list 'or e1 e2)) args))
-
-(defn andexp [& args]
-  (reduce
-    (fn [e1 e2]
-      
-      (list 'and e1 e2)) args))
- 
-;redefined not exp
-(defn notexp [& args]
-  (reduce 
-   (fn [e1 e2]
-    (list not e1 or e2)) args ))
-  
-;Boolean Algebra Simplication HashMap.class Supplied
-  (def boolExpMap
-  (hash-map '(or true) true,
-    '(or false) false,
-    '(and true) true,
-    '(and false) false,
-    '(or x false) 'x,
-    '(or false x) 'x,
-    '(or true x) true,
-    '(or x true) true,
-    '(and x false) false,
-    '(and false x) false,
-    '(and x true) 'x,
-    '(and true x) 'x,
-    '(not false) true,
-      **--
-    '(not true) false,
-    '(or x y z) '(or x (or y z)),
-    '(and x y z) '(and x (and y z)),
-    '(not (and x y)) '(or (not x) (not y)),
-    '(not (or x y)) '(and (not x) (not y)))),
-
-   (defn evalexp 
-    (reduce [evalexp] simplify [e1][e2]
-   (reduce concat [booLExpMap] (simplify (bind-values blinding exp)))
+   }
+}
